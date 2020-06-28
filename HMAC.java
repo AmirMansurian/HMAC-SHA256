a4adc5e5905ddb3e7884850a3b27e7d01b2e6e73
Amir <coloner12@gmail.com>
2020-06-28 12:12:55 +0430

    built-in hmac test

--- HMAC.java
+++ HMAC.java
@@ -1,6 +1,12 @@
 
 package hmac;
 
+import java.math.BigInteger;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import javax.crypto.Mac;
+import javax.crypto.spec.SecretKeySpec;
+
 /**
  *
  * @author amir
@@ -8,8 +14,19 @@ package hmac;
 public class HMAC {
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws NoSuchAlgorithmException, InvalidKeyException {
         
+        String Key = "SecretKey";
+        String message = "this is message";
+        
+        Mac hmac = Mac.getInstance("HmacSHA256");
+        SecretKeySpec secret_key = new SecretKeySpec(Key.getBytes(), "HmacSHA256");
+        hmac.init(secret_key);
+        byte[] hashed = hmac.doFinal(message.getBytes());
+        
+        String format = String.format("%032x", new BigInteger(1, hashed));
+        
+        System.out.println(format);
     }
     
 }
